bd260639181b31bfb5d6fcf8d9e8d36ad45507f7
faheem <faheemxxx456@gmail.com>
2026-09-04 12:24:17 +0530

    feat: complete base server setup with postgres health check and logging

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const pool= require('./src/configs/db');
+require('dotenv').config();
+
+const app= express();
+
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.url}`);
+      next()
+})
+
+app.get('/health', async (req,res,next)=>{
+    try{
+        const result = await pool.query('select now()')
+        res.json({ db_time: result.rows[0].now})
+    }catch(err){
+        console.error('DB Error:',err.message);
+        res.status(500).json({error:'Database connection failed '});
+    }
+
+});
+
+
+const PORT= process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
